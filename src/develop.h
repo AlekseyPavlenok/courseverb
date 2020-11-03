5d44d00fef938d0f7e0b07114133523f9f7db773
Kuvert <Quotes.kuvert@gmail.com>
2020-11-03 19:51:59 +0700

    Implemented the develop.h library

---
+++ src/develop.h
@@ -0,0 +1,16 @@
+#ifndef DEVELOP_H
+#define DEVELOP_H
+
+//#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//#include <fstream>
+#include <locale.h>
+#include <strings.h>
+
+// using namespace std;
+
+void dev();
+
+#endif
