4a0775c30f4f1c00cb095e0063d7401132070bca
AlekseyPavlenok <alekseypavlenok43@gmail.com>
2020-11-03 20:54:34 +0700

    Additional tests were written using the c test.h library

--- test/tests.c
+++ test/tests.c
@@ -56,3 +56,51 @@ CTEST(enter, check_verb_empty)
     unsigned int exp = 1;
     ASSERT_EQUAL(real, exp);
 }
+
+CTEST(enter, Exit_if_0)
+{
+    char s = '0';
+    int real = Exit(s);
+    unsigned int exp = 1;
+    ASSERT_EQUAL(real, exp);
+}
+
+CTEST(enter, Exit_if_3)
+{
+    char s = '3';
+    int real = Exit(s);
+    unsigned int exp = 0;
+    ASSERT_EQUAL(real, exp);
+}
+
+CTEST(enter, check_number_if_minus_1)
+{
+    int s = -1;
+    int real = check_number(s);
+    unsigned int exp = 0;
+    ASSERT_EQUAL(real, exp);
+}
+
+CTEST(enter, check_number_if_0)
+{
+    int s = 0;
+    int real = check_number(s);
+    unsigned int exp = 0;
+    ASSERT_EQUAL(real, exp);
+}
+
+CTEST(enter, check_number_normal)
+{
+    int s = 30;
+    int real = check_number(s);
+    unsigned int exp = 1;
+    ASSERT_EQUAL(real, exp);
+}
+
+CTEST(enter, check_number_big)
+{
+    int s = 9555555;
+    int real = check_number(s);
+    unsigned int exp = 0;
+    ASSERT_EQUAL(real, exp);
+}
